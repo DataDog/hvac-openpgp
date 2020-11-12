a9acd9805446e178c42324c75ca309f92a39339e
Trishank Karthik Kuppusamy <trishank.kuppusamy@datadoghq.com>
2020-11-12 10:30:48 -0500

    Switch orgs (#7)
    * switch orgs
    * fix test script
    
    Signed-off-by: Trishank Karthik Kuppusamy <trishank.kuppusamy@datadoghq.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,11 +5,12 @@ set -x
 export VAULT_ADDR="http://127.0.0.1:8200"
 export VAULT_TOKEN=root
 
-GOBIN=$PWD/bin go get github.com/trishankatdatadog/vault-gpg-plugin
+GOBIN=$PWD/bin go get github.com/DataDog/vault-gpg-plugin
 
 vault server -dev -dev-root-token-id=$VAULT_TOKEN -dev-plugin-dir=bin &
 VAULT_PID=$!
-vault login root
+
+sleep 5
 vault secrets enable vault-gpg-plugin
 
 python3 -m pip install -U tox
